0c232512cadbcc6280ae622b0de8400f1142f68c
andris310 <andris310@gmail.com>
2013-06-19 22:46:55 -0700

    ran rake db:migrate

--- Gemfile
+++ Gemfile
@@ -1,19 +1,15 @@
 source 'https://rubygems.org'
-
 gem 'rails', '3.2.13'
-
 ruby '1.9.3'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
-
 gem 'pg'
 gem 'devise'
 gem 'nokogiri'
 gem 'pry'
 gem 'geocoder'
 gem 'crack'
-
 
 # Gems used only for assets and not required
 # in production environments by default.
@@ -29,14 +25,12 @@ group :development, :test do
   gem 'capybara'
 end
 
-
 group :assets do
   gem 'sass-rails',   '~> 3.2.3'
   gem 'coffee-rails', '~> 3.2.1'
 
   # See https://github.com/sstephenson/execjs#readme for more supported runtimes
   # gem 'therubyracer', :platforms => :ruby
-
   gem 'uglifier', '>= 1.0.3'
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130614003101) do
+ActiveRecord::Schema.define(:version => 20130617235035) do
 
   create_table "details", :force => true do |t|
     t.string   "tracking_detail"
@@ -31,6 +31,12 @@ ActiveRecord::Schema.define(:version => 20130614003101) do
     t.datetime "updated_at",       :null => false
     t.boolean  "delivered"
     t.string   "status"
+  end
+
+  create_table "single_items", :force => true do |t|
+    t.string   "tracking_id"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
   end
 
   create_table "users", :force => true do |t|
